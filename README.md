58810d87d5cc9c682a7c05ea95b50b65e7916d2b
Paul Frazee <pfrazee@gmail.com>
2014-12-24 12:52:18 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Suggest Box
+
+Decorates a textarea with GitHub-style suggestion popups (eg for emojis and users)
+
+![Screenshot](/screenshot.png?raw=true)
+
+## Usage
+
+Suggest box is a decorator; it takes in a textarea element and binds to its events. The second param is object of options to suggest after an initial character has been typed.
+
+```js
+var textarea = document.querySelector('textarea.my-text-area')
+suggestBox(textarea, {
+  '@': [ // the initial character to watch for
+    {
+      title: 'Bob',              // title to render
+      // image: '/img/user.png', // alternative to title, shows the image instead
+      subtitle: 'Bob Roberts'    // subtitle to render
+      value: 'bob'               // value to insert (after the initial character) once selected
+    },
+    {
+      title: 'Alice',
+      subtitle: 'Alice Allison',
+      value: 'alice'
+    }
+  ]
+})
+```
+
+This example will watch for the '@' symbol and begin suggesting usernames (bob or alice).
+
+## Styles
+
+You must add your own styles to the page. Here is a some recommended styling in less:
+
+```css
+.suggest-box {
+  position: absolute;
+  border: 1px solid #ddd;
+  z-index: 100;
+  background: white;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    li {
+      padding: 4px 8px;
+      font-size: 85%;
+      border-bottom: 1px solid #ddd;
+      &:last-child {
+        border: 0;
+      }
+      &.selected {
+        color: #fff;
+        background-color: #428bca;
+        border-color: darken(#428bca, 5%);
+      }
+      img {
+        height: 20px;
+      }
+    }
+  }
+}
+```
+
+## Acknowledgements
+
+Uses [textarea-caret-position](https://github.com/component/textarea-caret-position), MIT Licensed, Copyright (c) 2014 Jonathan Ong me@jongleberry.com
+
+## License
+
+MIT Licensed, Copyright 2014 Paul Frazee
---
+++ index.js
@@ -0,0 +1,288 @@
+'use strict'
+var h = require('hyperscript')
+var wordBoundary = /\s/
+
+module.exports = function(el, options) {
+  var box = {
+    options: options,
+    active: false,
+    activate: activate,
+    deactivate: deactivate,
+    update: update
+  }
+  el.addEventListener('input', oninput.bind(box))
+  el.addEventListener('keydown', onkeydown.bind(box))
+  el.addEventListener('blur', onblur.bind(box))
+  return box
+}
+
+function render(box) {
+  return h('.suggest-box', { style: { left: (box.x+'px'), top: (box.y+'px') } }, [
+    h('ul', renderOpts(box))
+  ])
+}
+
+function renderOpts(box) {
+  var fragment = document.createDocumentFragment()
+  for (var i=0; i < box.filtered.length; i++) {
+    var opt = box.filtered[i]
+    var tag = 'li'
+    if (i === box.selection) tag += '.selected'
+    var title = opt.image ? h('img', { src: opt.image }) : h('strong', opt.title)
+    fragment.appendChild(h(tag, [title, ' ', h('small', opt.subtitle)]))
+  }
+  return fragment
+}
+
+function activate() {
+  if (this.active)
+    return
+  this.active = true
+  this.el = render(this)
+  document.body.appendChild(this.el)
+}
+
+function update() {
+  if (!this.active)
+    return
+  var ul = this.el.querySelector('ul')
+  ul.innerHTML = ''
+  ul.appendChild(renderOpts(this))
+}
+
+function deactivate() {
+  if (!this.active)
+    return
+  this.el.parentNode.removeChild(this.el)
+  this.el = null
+  this.active = false
+}
+
+function oninput(e) {
+  var options
+
+  // are we in a word that starts with one of the starting characters?
+  var v = e.target.value
+  var i = e.target.selectionStart - 1
+  // seek backwards from the cursor
+  for (i; i >= 0; i--) {
+    var c = v.charAt(i)
+    // hit a word boundary?
+    if (wordBoundary.test(c))
+      return this.deactivate()
+    // hit a starting character?
+    if (c in this.options && (i === 0 || wordBoundary.test(v.charAt(i - 1)))) {
+      options = this.options[c]
+      break
+    }
+  }
+  // no hit?
+  if (i < 0)
+    return this.deactivate()
+
+  // extract current word
+  var word = v.slice(i+1, e.target.selectionStart)
+  if(!word)
+    return this.deactivate()
+
+  // filter and order the list by the current word
+  this.selection = 0
+  this.filtered = []
+  for (var i=0; i < options.length; i++) {
+    var opt = options[i]
+    var title = opt.title.indexOf(word)
+    var subtitle = opt.subtitle.indexOf(word)
+    var rank = (title === -1 ? subtitle : (subtitle === -1 ? title : Math.min(title, subtitle)))
+    if(rank > -1) {
+      opt.rank = rank
+      this.filtered.push(opt)
+    }
+  }
+
+  this.filtered = this.filtered.sort(compare).slice(0, 20)
+
+  // cancel if there's nothing available
+  if (this.filtered.length == 0)
+    return this.deactivate()
+
+  // create / update the element
+  if (this.active) {
+    this.update()
+  } else {
+    // calculate position
+    var pos = textareaCaretPosition(e.target, i)
+    var rects = e.target.getClientRects()
+    pos.left += rects[0].left
+    pos.top += rects[0].top + 20
+
+    // setup
+    this.x = pos.left
+    this.y = pos.top
+    this.activate()
+  }
+}
+
+function compare(a, b) {
+  return compareval(a.rank, b.rank) || compareval(a.title, b.title)
+}
+
+function compareval(a, b) {
+  return a === b ? 0 : a < b ? -1 : 1
+}
+
+function onkeydown(e) {
+  if (this.active) {
+    var sel = this.selection
+
+    if (e.keyCode == 38 || e.keyCode == 40 || e.keyCode == 13 || e.keyCode == 9|| e.keyCode == 27)
+      e.preventDefault()
+
+    // up
+    if (e.keyCode == 38 && sel > 0) {
+      this.selection = sel - 1
+      this.update()
+    }
+
+    // down
+    if (e.keyCode == 40 && sel < (this.filtered.length - 1)) {
+      this.selection = sel + 1
+      this.update()
+    }
+
+    // escape
+    if (e.keyCode == 27)
+      this.deactivate()
+
+    // enter or tab
+    if (e.keyCode == 13 || e.keyCode == 9) {
+      if (this.filtered.length) {
+        var choice = this.filtered[sel]
+        if (choice && choice.value) {
+          // update the text under the cursor to have the current selection's value
+          var v = e.target.value
+          var start = e.target.selectionStart
+          var end = start
+          for (start; start >= 0; start--) {
+            if (v.charAt(start) in this.options)
+              break
+          }
+          for (end; end < v.length; end++) {
+            if (wordBoundary.test(v.charAt(end)))
+              break
+          }
+          e.target.value = v.slice(0, start + 1) + choice.value + ' ' + v.slice(end)
+          // fire the change event
+          //fireEvent(e.target, 'change')
+        }
+      }
+      this.deactivate()
+    }
+  }
+}
+
+function onblur(e) {
+  this.deactivate()
+}
+
+// https://github.com/component/textarea-caret-position
+// MIT Licensed, Copyright (c) 2014 Jonathan Ong me@jongleberry.com
+
+// The properties that we copy into a mirrored div.
+// Note that some browsers, such as Firefox,
+// do not concatenate properties, i.e. padding-top, bottom etc. -> padding,
+// so we have to do every single property specifically.
+var properties = [
+  'direction',  // RTL support
+  'boxSizing',
+  'width',  // on Chrome and IE, exclude the scrollbar, so the mirror div wraps exactly as the textarea does
+  'height',
+  'overflowX',
+  'overflowY',  // copy the scrollbar for IE
+
+  'borderTopWidth',
+  'borderRightWidth',
+  'borderBottomWidth',
+  'borderLeftWidth',
+
+  'paddingTop',
+  'paddingRight',
+  'paddingBottom',
+  'paddingLeft',
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/font
+  'fontStyle',
+  'fontVariant',
+  'fontWeight',
+  'fontStretch',
+  'fontSize',
+  'fontSizeAdjust',
+  'lineHeight',
+  'fontFamily',
+
+  'textAlign',
+  'textTransform',
+  'textIndent',
+  'textDecoration',  // might not make a difference, but better be safe
+
+  'letterSpacing',
+  'wordSpacing'
+];
+
+var isFirefox = !(window.mozInnerScreenX == null);
+
+function textareaCaretPosition(element, position) {
+  // mirrored div
+  var div = document.createElement('div');
+  div.id = 'input-textarea-caret-position-mirror-div';
+  document.body.appendChild(div);
+
+  var style = div.style;
+  var computed = window.getComputedStyle? getComputedStyle(element) : element.currentStyle;  // currentStyle for IE < 9
+
+  // default textarea styles
+  style.whiteSpace = 'pre-wrap';
+  if (element.nodeName !== 'INPUT')
+    style.wordWrap = 'break-word';  // only for textarea-s
+
+  // position off-screen
+  style.position = 'absolute';  // required to return coordinates properly
+  style.visibility = 'hidden';  // not 'display: none' because we want rendering
+
+  // transfer the element's properties to the div
+  for (var i=0; i < properties.length; i++) {
+    var prop = properties[i]
+    style[prop] = computed[prop]
+  }
+
+  if (isFirefox) {
+    style.width = parseInt(computed.width) - 2 + 'px'  // Firefox adds 2 pixels to the padding - https://bugzilla.mozilla.org/show_bug.cgi?id=753662
+    // Firefox lies about the overflow property for textareas: https://bugzilla.mozilla.org/show_bug.cgi?id=984275
+    if (element.scrollHeight > parseInt(computed.height))
+      style.overflowY = 'scroll';
+  } else {
+    style.overflow = 'hidden';  // for Chrome to not render a scrollbar; IE keeps overflowY = 'scroll'
+  }  
+
+  div.textContent = element.value.substring(0, position);
+  // the second special handling for input type="text" vs textarea: spaces need to be replaced with non-breaking spaces - http://stackoverflow.com/a/13402035/1269037
+  if (element.nodeName === 'INPUT')
+    div.textContent = div.textContent.replace(/\s/g, "\u00a0");
+
+  var span = document.createElement('span');
+  // Wrapping must be replicated *exactly*, including when a long word gets
+  // onto the next line, with whitespace at the end of the line before (#7).
+  // The  *only* reliable way to do that is to copy the *entire* rest of the
+  // textarea's content into the <span> created at the caret position.
+  // for inputs, just '.' would be enough, but why bother?
+  span.textContent = element.value.substring(position) || '.';  // || because a completely empty faux span doesn't render at all
+  div.appendChild(span);
+
+  var coordinates = {
+    top: span.offsetTop + parseInt(computed['borderTopWidth']),
+    left: span.offsetLeft + parseInt(computed['borderLeftWidth'])
+  };
+
+  document.body.removeChild(div);
+
+  return coordinates;
+}
